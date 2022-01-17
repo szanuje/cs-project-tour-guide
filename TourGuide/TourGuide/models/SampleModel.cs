72111c80c6465ad5d37f0245ecd556fdbe03538d
Konrad Ulman <veradopl@gmail.com>
2022-01-17 22:00:06 +0100

    Add inmemory db

---
+++ TourGuide/TourGuide/models/SampleModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TourGuide.models
+{
+    public class SampleDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public SampleDbContext()
+        {
+        }
+
+        public SampleDbContext(DbContextOptions<SampleDbContext> options) : base(options)
+        {
+        }
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+    }
+}
