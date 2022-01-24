9bbebce6da8bec0a9b3b7f96d583dff6b7f3e96a
pajelonek <pawel.jelonek.krakow@gmail.com>
2022-01-24 16:31:33 +0100

    Refactor of datainit

--- TourGuide/TourGuide.Domain/Data/DataInit.cs
+++ TourGuide/TourGuide.Domain/Data/DataInit.cs
@@ -44,81 +44,136 @@ namespace TourGuide.Domain.Data
                 db.SaveChanges();
             }
 
-            /*var destinationSpain = db.Destinations
+            var destinationSpain = db.Destinations
             .Where(d => d.Name == "Hiszpania")
             .FirstOrDefault();
 
             if (destinationSpain == null)
             {
-                var newDestination = new Destination() { Name = "Hiszpania", Description = "Państwo w zachodniej części Europy, położone na Półwyspie Iberyjskim." };
-                var address = new Address() { City = "Barcelona", Country = "Hiszpania", HouseNumber = 1, PostalCode = 08002, Street = "Pla de la Seu" };
-                var place = new Place() { Name = "Katedra św. Eulalii ", Description = "Jeden z cenniejszych przykładów architektury gotyckiej w Hiszpanii. ", Destination = newDestination, Address = address };
-                db.Add(address);
-
-                var address2 = new Address() { City = "Barcelona", Country = "Hiszpania", HouseNumber = 12, PostalCode = 08028, Street = "C. d'Arístides Maillol" };
-                var place2 = new Place() { Name = "Camp Nou", Description = "Stadion piłkarski w Barcelonie w Hiszpanii, na którym są rozgrywane mecze FC Barcelony.", Destination = newDestination, Address = address };
-                db.Add(address2);
-
-                db.Add(place);
-                db.SaveChanges();
-                newDestination.Places.Add(place);
-                newDestination.Places.Add(place2);
-
-                db.Add(newDestination);
+                db.Add(new Destination
+                {
+                    Name = "Hiszpania",
+                    Description = "Państwo w zachodniej części Europy, położone na Półwyspie Iberyjskim.",
+                    Places = new List<Place>()
+                    {
+                        new Place()
+                        {
+                            Name = "Katedra św. Eulalii",
+                            Description = "Jeden z cenniejszych przykładów architektury gotyckiej w Hiszpanii. ",
+                            Address = new Address
+                            {
+                                City = "Barcelona",
+                                Country = "Hiszpania",
+                                HouseNumber = 1,
+                                PostalCode = "08002",
+                                Street = "Pla de la Seu"
+                            }
+                        },
+                        new Place()
+                        {
+                            Name = "Camp Nou",
+                            Description = "Jeden z cenniejszych przykładów architektury gotyckiej w Hiszpanii. ",
+                            Address = new Address
+                            {
+                                City = "Barcelona",
+                                Country = "Hiszpania",
+                                HouseNumber = 12,
+                                PostalCode = "08028",
+                                Street = "C. d'Arístides Maillol"
+                            }
+                        }
+                    }
+                });
 
                 db.SaveChanges();
             }
-
+            
             var destinationPortugal = db.Destinations
             .Where(d => d.Name == "Portugalia")
             .FirstOrDefault();
 
             if (destinationPortugal == null)
             {
-                var portugalDestination = new Destination() { Name = "Portugalia", Description = "Najdalej wysunięte na zachód państwo Europy." };
-                var address = new Address() { City = "Lizbona", Country = "Portugalia", HouseNumber = 1, PostalCode = 140038, Street = "Av. Brasília" };
-                var place = new Place() { Name = "Pomnik Odkrywców", Description = "Monumentalny pomnik, znajdujący się w lizbońskiej dzielnicy Belém.", Destination = portugalDestination, Address = address };
-                db.Add(address);
-
-                var address2 = new Address() { City = "Lizbona", Country = "Portugalia", HouseNumber = 1, PostalCode = 1990005, Street = "Esplanada Dom Carlos I" };
-                var place2 = new Place() { Name = "Oceanarium w Lizbonie", Description = "Największe oceanarium w Europie, w którym żyje przeszło 25 tysięcy morskich stworzeń z całego świata.", Destination = portugalDestination, Address = address };
-                db.Add(address2);
-
-                db.Add(place);
-                db.SaveChanges();
-                portugalDestination.Places.Add(place);
-                portugalDestination.Places.Add(place2);
-
-                db.Add(portugalDestination);
+                db.Add(new Destination
+                {
+                    Name = "Portugalia",
+                    Description = "Najdalej wysunięte na zachód państwo Europy.",
+                    Places = new List<Place>()
+                    {
+                        new Place()
+                        {
+                            Name = "Pomnik Odkrywców",
+                            Description = "Monumentalny pomnik, znajdujący się w lizbońskiej dzielnicy Belém.",
+                            Address = new Address
+                            {
+                                City = "Lizbona",
+                                Country = "Portugalia",
+                                HouseNumber = 1,
+                                PostalCode = "140038",
+                                Street = "Av. Brasília"
+                            }
+                        },
+                        new Place()
+                        {
+                            Name = "Oceanarium w Lizbonie",
+                            Description = "Największe oceanarium w Europie, w którym żyje przeszło 25 tysięcy morskich stworzeń z całego świata.",
+                            Address = new Address
+                            {
+                                City = "Lizbona",
+                                Country = "Portugalia",
+                                HouseNumber = 1,
+                                PostalCode = "1990005",
+                                Street = "Esplanada Dom Carlos I"
+                            }
+                        }
+                    }
+                });
 
                 db.SaveChanges();
             }
 
-
             var destinationEngland = db.Destinations
-            .Where(d => d.Name == "Portugalia")
+            .Where(d => d.Name == "Anglia")
             .FirstOrDefault();
 
             if (destinationEngland == null)
             {
-                var newDestination = new Destination() { Name = "Anglia", Description = "Kraj stanowiący część Zjednoczonego Królestwa Wielkiej Brytanii i Irlandii Północnej." };
-                var address = new Address() { City = "Londyn", Country = "Anglia", HouseNumber = 1, PostalCode = 0, Street = "Great Russell St" };
-                var place = new Place() { Name = "Muzeum Brytyjskie", Description = "Muzeum narodowe z siedzibą w Londynie, największe muzeum Wielkiej Brytanii i jedno z największych na świecie", Destination = newDestination, Address = address };
-                db.Add(address);
-
-                var address2 = new Address() { City = "Londyn", Country = "Anglia", HouseNumber = 1, PostalCode = 0, Street = "Riverside Building" };
-                var place2 = new Place() { Name = "London Eye", Description = "Koło obserwacyjne znajdujące się w dzielnicy Lambeth w Londynie, na południowym brzegu Tamizy, między mostami Westminster i Hungerford.", Destination = newDestination, Address = address };
-                db.Add(address2);
-
-                db.Add(place);
+                db.Add(new Destination
+                {
+                    Name = "Anglia",
+                    Description = "Kraj stanowiący część Zjednoczonego Królestwa Wielkiej Brytanii i Irlandii Północnej.",
+                    Places = new List<Place>()
+                    {
+                        new Place()
+                        {
+                            Name = "Muzeum Brytyjskie",
+                            Description = "Monumentalny pomnik, znajdujący się w lizbońskiej dzielnicy Belém.",
+                            Address = new Address
+                            {
+                                City = "Londyn",
+                                Country = "Anglia",
+                                HouseNumber = 1,
+                                PostalCode = "0",
+                                Street = "Great Russell St"
+                            }
+                        },
+                        new Place()
+                        {
+                            Name = "London Eye",
+                            Description = "Największe oceanarium w Europie, w którym żyje przeszło 25 tysięcy morskich stworzeń z całego świata.",
+                            Address = new Address
+                            {
+                                City = "Londyn",
+                                Country = "Anglia",
+                                HouseNumber = 1,
+                                PostalCode = "0",
+                                Street = "Riverside Building"
+                            }
+                        }
+                    }
+                });
                 db.SaveChanges();
-                newDestination.Places.Add(place);
-                newDestination.Places.Add(place2);
-
-                db.Add(newDestination);
-
-                db.SaveChanges();
-            }*/
+            }
         }
     }
 }
